c6697c9d60820abae27d8f258ca4b18a2e73ccb6
Chentao <896263031@qq.com>
2017-11-28 09:04:13 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     //个人页面的展示
     public function show(User $user)
     {
@@ -18,12 +23,14 @@ class UsersController extends Controller
     //编辑个人
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     //表单提交
     public function update(UserRequest $request, ImageUploadHandler $uploader,User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
