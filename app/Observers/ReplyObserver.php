f5f7cfc33540ab001ac3c3994f5d6119acd2d032
ChenTao <896263031@qq.com>
2017-12-16 03:09:39 +0000

    Notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -12,7 +13,13 @@ class ReplyObserver
     //使用模型监控created事件，当模型数据成功创建时，方法被调用
     public function created(Reply $reply)
     {
-        $reply->topic->increment('reply_count', 1);
+        $topic = $reply->topic;
+        $topic->increment('reply_count', 1);
+
+        // 如果评论的作者不是话题的作者，才需要通知
+        if ( ! $reply->user->isAuthorOf($topic)) {
+            $topic->user->notify(new TopicReplied($reply));
+        }
     }
 
 //    public function creating(Reply $reply)
